a6c3b9f98c91e2580b887c157930f56c40c9a8db
Taiany Martins <taiany22@yahoo.com.br>
2022-10-29 16:59:07 -0300

    Adicionando Entidades

---
+++ LabSysCloud/LabSysCloud.Domain/Entities/EntidadeBase.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LabSysCloud.Domain.Entities
+{
+    public class EntidadeBase
+    {
+        public virtual long Id { get; set; }
+    }
+}
